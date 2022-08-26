116ba2f6dc4a0f50f8ca69994b23b3d803aa5366
Udit8158 <kunduudit8158@gmail.com>
2022-08-26 14:42:14 +0530

    Change in a shortcut

--- rc.lua
+++ rc.lua
@@ -118,7 +118,7 @@ local modkey1      = "Control"
 
 -- personal variables
 --change these variables if you want
-local browser1          = "vivaldi-stable"
+local browser1          = "brave"
 local browser2          = "firefox"
 local browser3          = "chromium -no-default-browser-check"
 local editor            = os.getenv("EDITOR") or "nano"
